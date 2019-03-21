31e1283c673c0bb72fd849914290d4d873e354a7
Shiqing Wu <wukonatan@gmail.com>
2019-03-22 11:26:46 +1300

    Initial commit

---
+++ UserAgent.py
@@ -0,0 +1,13 @@
+
+
+class Action:
+    def __init__(self, id, preference, benefit):
+        self.id = id
+        self.preference = preference
+        self.benefit = benefit
+
+class UserAgent:
+    def __init__(self):
+        self.id = id
+        self.preference = []
+        self.action = 0
